79d5a3e89e05d882397f8e73e92b9aabd6e7b527
justagut <jortsanderman@gmail.com>
2025-11-24 21:12:42 +0100

    i made it bounce a lot better

--- src/main/java/com/justagut/MinigameMod/entity/custom/BallBasics.java
+++ src/main/java/com/justagut/MinigameMod/entity/custom/BallBasics.java
@@ -18,8 +18,6 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.Nullable;
 
-import static java.lang.Math.abs;
-
 public class BallBasics extends Monster {
     public final AnimationState idleAnimationState = new AnimationState();
     public final AnimationState walkAnimationState = new AnimationState();
--- src/main/java/com/justagut/MinigameMod/entity/custom/TennisBall.java
+++ src/main/java/com/justagut/MinigameMod/entity/custom/TennisBall.java
@@ -1,4 +1,11 @@
 package com.justagut.MinigameMod.entity.custom;
 
-public class TennisBall {
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.level.Level;
+
+public class TennisBall extends BallBasics{
+    public TennisBall(EntityType<? extends Monster> entityType, Level level) {
+        super(entityType, level);
+    }
 }
