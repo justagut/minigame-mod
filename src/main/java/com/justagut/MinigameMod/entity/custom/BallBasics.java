f579a6c7cb3436ef35d673d7d952e1101e91f0da
justagut <jortsanderman@gmail.com>
2026-04-02 08:59:31 +0200

    trying to add the model and renderer. didnt yet succeed but working on it

--- src/main/java/com/justagut/MinigameMod/entity/custom/BallBasics.java
+++ src/main/java/com/justagut/MinigameMod/entity/custom/BallBasics.java
@@ -2,6 +2,7 @@ package com.justagut.MinigameMod.entity.custom;
 
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Position;
 import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.entity.AnimationState;
 import net.minecraft.world.entity.EntityType;
@@ -14,15 +15,25 @@ import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.ServerLevelAccessor;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Objects;
+
 public class BallBasics extends Monster {
     public Vec3 oldvelocity = new Vec3(0,0,0);
-    public float drag = 1.09F;
-    public float weight = 1.01f;
-    public float bouncyness = 0.9f;
+    public float drag = 0.95F;
+    public float weight = 1f;
+    public float bouncyness = 0.8f;
+    public float gravitystrenght = 0.05f;
+    public BlockPos oldpos = new BlockPos(0,1000,0);
+    public BlockPos newpos = new BlockPos(0,0,0);
+    public BlockPos diffpos = new BlockPos(0,0,0);
+    Level level = this.level();
+    public boolean xcol = false;
+    public boolean zcol = false;
 
     public BallBasics(EntityType<? extends Monster> entityType, Level level) {
         super(entityType, level);
@@ -33,9 +44,7 @@ public class BallBasics extends Monster {
     public boolean fireImmune() {
         return true;
     }
-    public void weight(Float counterweight){
-        weight = counterweight;
-    }
+    public void weight(Float counterweight){weight = counterweight;}
     public void drag(Float counterdrag){
         drag = counterdrag;
     }
@@ -49,9 +58,7 @@ public class BallBasics extends Monster {
     }
 
     @Override
-    protected void registerGoals() {
-            this.goalSelector.addGoal(1, new FloatGoal(this));
-            }
+    protected void registerGoals() {this.goalSelector.addGoal(1, new FloatGoal(this));}
     @Override
     protected float getJumpPower() {
         return 0.4F; // default is around 0.42F for most mobs
@@ -74,20 +81,28 @@ public class BallBasics extends Monster {
     public void tick() {
         if (this.verticalCollision){
             this.setDeltaMovement(oldvelocity.x,-oldvelocity.y*bouncyness - 0.01,oldvelocity.z);
-        } else if (this.horizontalCollision) {
-            if (this.getDeltaMovement().x-oldvelocity.x>0.1){
-                this.setDeltaMovement(-oldvelocity.x*bouncyness, oldvelocity.y, oldvelocity.z);
-            } else{
-                this.setDeltaMovement(oldvelocity.x, oldvelocity.y, -oldvelocity.z*bouncyness);
+        }
+        if (this.horizontalCollision && oldpos!= new BlockPos(0, 1000, 0)) {
+            newpos = this.blockPosition();
+            oldpos = new BlockPos(oldpos.getX(),newpos.getY(),oldpos.getZ());
+            diffpos = new BlockPos
+                    (newpos.getX() - oldpos.getX(), 0, newpos.getZ() - oldpos.getZ());
+            xcol = diffpos.getX() != 0 && level.getBlockState(new BlockPos(oldpos.getX(), newpos.getY(), newpos.getZ())).isSolid();
+            zcol = diffpos.getZ() != 0 && level.getBlockState(new BlockPos(newpos.getX(), newpos.getY(), oldpos.getZ())).isSolid();
+            if ((!xcol && !zcol) || (xcol && zcol)){
+                this.setDeltaMovement(-oldvelocity.x*bouncyness,oldvelocity.y,-oldvelocity.z*bouncyness);
+            }
+            if (!xcol && zcol){
+                this.setDeltaMovement(oldvelocity.x,-oldvelocity.y,-oldvelocity.z*bouncyness);
+            }
+            if (xcol && !zcol){
+                this.setDeltaMovement(-oldvelocity.x*bouncyness,-oldvelocity.y,oldvelocity.z);
             }
         }
         oldvelocity = this.getDeltaMovement();
         super.tick();
-        this.setDeltaMovement(getDeltaMovement().multiply(drag,weight,drag));
-
-
-
+        this.setDeltaMovement(oldvelocity.multiply(drag,weight,drag));
+        this.setDeltaMovement(getDeltaMovement().x,getDeltaMovement().y - gravitystrenght,getDeltaMovement().z);
+        oldpos = this.blockPosition();
     }
-
-
 }
